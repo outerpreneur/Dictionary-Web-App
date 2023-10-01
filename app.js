bf40d8828a5de93636f2f5d4e81ddc03a86b8577
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-10-02 00:41:41 +0200

    Werb

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
+// Global variables
+
 const searchInput = document.getElementById("search-input");
 const formSearch = document.getElementById("form-search");
+const searchIcon = document.getElementById("search-icon");
 const searchQuery = document.getElementById("query");
 const phoneticQuery = document.getElementById("phonetic");
 const phoneticSound = document.getElementById("phonetic-sound");
 const baseUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const synonyms = document.getElementById("synonyms");
+
+// triggers
 
 formSearch.addEventListener("submit", (event) => {
   event.preventDefault();
-  searchInputValueString = searchInput.value.toString();
   searchQuery.innerText = capitalizeFirstLetter(searchInput.value);
-  const completeUrl = baseUrl + searchInputValueString;
+  fetchDefinitions();
+  fetchVerbs();
+});
 
+searchIcon.addEventListener("click", (event) => {
+  event.preventDefault();
+  fetchDefinitions();
+});
+
+// functions
+
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function fetchDefinitions(definition) {
+  const searchInputValueString = searchInput.value.toString();
+  const completeUrl = baseUrl + searchInputValueString;
   fetch(completeUrl)
     .then((response) => response.json())
     .then((data) => {
@@ -18,30 +39,30 @@ formSearch.addEventListener("submit", (event) => {
       const wordArr = data[0].meanings;
       const nounElement = document.getElementById("definitions-list");
       for (arr of wordArr) {
-        console.log(arr.definitions[0].definition);
+        // console.log(arr.definitions[0].definition);
         const definitionParagraph = document.createElement("li");
         definitionParagraph.textContent = arr.definitions[0].definition;
-        definitionParagraph.classList.add("my-3");
         nounElement.appendChild(definitionParagraph);
+        definitionParagraph.classList.add("my-3");
       }
+      synonyms.innerHTML = data[0].meanings[0].synonyms;
       document.querySelector("form").reset();
     });
-});
-
-function capitalizeFirstLetter(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-// phoneticSound.addEventListener("click", (event) => {
-//   const searchInputValueString = searchInput.value.toString();
-//   const completeUrl = baseUrl + searchInputValueString;
-//   // fetch("https://api.dictionaryapi.dev/api/v2/entries/en/dog")
-//   //   .then((response) => response.json())
-//   //   .then((data) => {
-//   // const audioElement = data[0].phonetics[2].audio;
-//   const audioUrl =
-//     "https://api.dictionaryapi.dev/media/pronunciations/en/dog-us.mp3";
-//   phoneticSound.src = audioUrl;
-//   phoneticSound.play();
-//   // });
-// });
+function fetchVerbs(verbs) {
+  const searchInputValueString = searchInput.value.toString();
+  const completeUrl = baseUrl + searchInputValueString;
+  fetch(completeUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const verbArray = data[0].meanings;
+      const verbsList = document.getElementById("verbs-list");
+      for (verb of verbArray) {
+        const verbParagraph = document.createElement("li");
+        verbParagraph.textContent = verb.definitions[1].definition;
+        verbsList.appendChild(verbParagraph);
+        verbParagraph.classList.add("my-3");
+      }
+    });
+}
